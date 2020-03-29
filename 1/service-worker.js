acfff404a59ade983af16221a34e250da0f2dc19
luidimso <luidi_mso@outlook.com>
2020-03-29 18:51:22 -0300

    Activating cache

--- 1/service-worker.js
+++ 1/service-worker.js
@@ -8,3 +8,17 @@ self.addEventListener('install', function(event){
       return cache.addAll(urlsToCache);
   }));
 });
+
+self.addEventListener('activate', function(event){
+  console.log(event);
+
+  var cacheWhiteList = [CACHE_NAME];
+
+  return event.waitUntil(self.caches.keys().then(function(cacheNames){
+    return Promise.all(cacheNames.map(function(cacheName){
+      if(cacheWhiteList.indexOf(cacheName) == -1){
+        return self.caches.delete(cacheName);
+      }
+    }))
+  }))
+});
